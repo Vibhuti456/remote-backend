cd331abca2279856ec0a6ef329f7929f9c2e7981
Vibhuti Jain <78954176+Vibhuti456@users.noreply.github.com>
2025-06-18 00:44:31 +0530

    Update confriguration.tf

--- confriguration.tf
+++ confriguration.tf
@@ -5,5 +5,11 @@ terraform {
       version = "6.0.0-beta3"
     }
   }
+ backend "s3" {
+    bucket = "vremote-backend-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "dynamo_db"    
+  }
 }
 
